c9623b4ba60c5cc237063153e8f70ec10dab44cd
Fuyukuniaki <info@a5-w.com>
2023-10-12 18:15:27 +0900

    231012 header

--- header.php
+++ header.php
@@ -10,13 +10,15 @@
 
 <body <?php body_class( 'l-body js-body' ); ?>>
 	<header id="top" class="l-header js-header">
-		<a class="p-header">
+		<a class="p-header__link">
 			<h1 href="/#top" class="c-logo">閃火</h1>
 		</a>
-		<dl class="p-hamburger-menu">
-			<dt class="c-icon__hamburger">≡</dt>
-			<dd class="p-hidden-menu">
-				<!-- menu -->
-			</dd>
-		</dl>
+		<nav class="p-header__nav">
+			<dl class="p-hamburger-menu">
+				<dt class="c-icon__hamburger">Tap to open</dt>
+				<dd class="p-hidden-menu">
+					<!-- menu -->
+				</dd>
+			</dl>
+		</nav>
 	</header>
