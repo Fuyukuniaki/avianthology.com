6892b4e7183fa522436a6036b5b68bb86855864e
Fuyukuniaki <info@a5-w.com>
2023-11-05 10:47:47 +0900

    231105 authorリスト 保存

--- footer.php
+++ footer.php
@@ -1,9 +1,7 @@
 		<footer class="l-footer">
-			<figure class="c-footer__img">
-				<img class="c-footer__img--logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="閃火">
-			</figure>
+			<p class="c-logo">閃火</p><br>
 			<small class="c-footer__copyright">2024 Akira Vasily Ignatov Anthology</small>
 		</footer>
-<?php wp_footer(); ?>
+		<?php wp_footer(); ?>
 	</body>
 </html>
--- front-page.php
+++ front-page.php
@@ -30,184 +30,317 @@
 		<article class="p-news">
 			<h2 class="c-news__title">news</h2>
 			<dl class="p-news-contents__wrap">
-				<dt class="c-news-contents__text">news news news news.</dt>
 				<dd class="c-news-contents__date">2023/XX/XX</dd>
-				<dt class="c-news-contents__text">news news news news.</dt>
+				<dt class="c-news-contents__text">この文章はダミーです。文字の大きさや字間、行間をみるために配置しています。</dt>
 				<dd class="c-news-contents__date">2023/XX/XX</dd>
+				<dt class="c-news-contents__text">この文章はダミーです。文字の大きさや字間、行間をみるために配置しています。この文章はダミーです。文字の大きさや字間、行間をみるために配置しています。</dt>
 			</dl>
 		</article>
-	<section class="l-about-part">
-		<figure class="c-bookcover__image--wrap">
-			<img class="c-bookcover__image" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="書影">
-		</figure>
-		<dl class="p-lists-about">
-			<dt class="c-lists-about__category">内容</dt>
-			<dd class="c-lists-about__content">
-				煇・ワシリー・イグナトフ中心<br>
-				全年齢アンソロジー<br>
-				<br>
-				イラスト・漫画・小説・写真など<br>
-				〇〇名のご寄稿者様それぞれの形で<br>
-				煇への想いを形にしていただく<br>
-				アンソロジーです。<br>
-				<br>
-				<span class="c-text__small">※生存if、過去ifなどを含みます。</span>
-			</dd>
-			<dt class="c-lists-about__category">発行日・イベント</dt>
-			<dd class="c-lists-about__content">2024年3月17日 HARU COMIC CITY 32 /東京</dd>
-			<dt class="c-lists-about__category">発行日・イベント</dt>
-			<dd class="c-lists-about__content">2024年3月17日 HARU COMIC CITY 32 /東京</dd>
-			<dt class="c-lists-about__category">発行日・イベント</dt>
-			<dd class="c-lists-about__content">2024年3月17日 HARU COMIC CITY 32 /東京</dd>
-			<dt class="c-lists-about__category">発行日・イベント</dt>
-			<dd class="c-lists-about__content">2024年3月17日 HARU COMIC CITY 32 /東京</dd>
-			<dt class="c-lists-about__category">発行日・イベント</dt>
-			<dd class="c-lists-about__content">2024年3月17日 HARU COMIC CITY 32 /東京</dd>
-		</dl>
-		<article class="p-author-list">
-			<h2 class="c-author-list__title">author list</h2>
-			<dl class="p-author-list__lists">
-				<dt class="c-author-list__lists--title">
-					イラスト
+		<article class="p-x-twitter">
+			<h2 class="c-x-twitter__title">X（twitter）</h2>
+			<section class="c-x-twitter__data">
+				<a class="twitter-timeline" href="https://twitter.com/AVIantholo2024?ref_src=twsrc%5Etfw">Tweets by AVIantholo2024</a>
+				<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+			</section>
+		</article>
+		<section class="l-about-part">
+			<figure class="c-bookcover__image--wrap">
+				<img class="c-bookcover__image" src="<?php echo get_template_directory_uri(); ?>/assets/images/img-about-cover.svg" alt="書影">
+			</figure>
+			<dl class="p-lists-about">
+			<dt class="c-lists-about__category">タイトル</dt>
+				<dd class="c-lists-about__content">『閃火』</dd>
+				<dt class="c-lists-about__category">内容</dt>
+				<dd class="c-lists-about__content">
+					「PSYCHO-PASS」シリーズ<br>
+					煇・ワシリー・イグナトフ中心<br>
+					非公式全年齢アンソロジー<br>
+					<br>
+					イラスト・漫画・小説・写真など<br>
+					主催含む35名の創作者が<br>
+					それぞれの形で煇への想いを表現する<br>
+					アンソロジーです。<br>
+					<br>
+					<span class="c-text__small">※生存if、過去ifなどを含みます。</span>
+				</dd>
+				<dt class="c-lists-about__category">発行日・イベント</dt>
+				<dd class="c-lists-about__content">2024年3月17日 HARU COMIC CITY 32 /東京</dd>
+				<dt class="c-lists-about__category">仕様</dt>
+				<dd class="c-lists-about__content">A5サイズ/カラー・モノクロ/ページ数未定</dd>
+				<dt class="c-lists-about__category">ノベルティ</dt>
+				<dd class="c-lists-about__content">COMING SOON</dd>
+				<dt class="c-lists-about__category">頒布価格</dt>
+				<dd class="c-lists-about__content">COMING SOON</dd>
+			</dl>
+			<article class="p-author-list">
+				<h2 class="c-author-list__title">author list</h2>
+				<p class="c-author-list__caption">クリックして&nbsp;寄稿者一覧・サンプルを開く</p>
+				<dl class="p-author-list__lists">
+					<dt class="c-author-list__lists--title">
+						イラスト
+					</dt>
+					<dd class="c-author-list__lists--wrap">
+						<ul class="c-author-list__lists--member">
+							<li class="c-author-list__lists--list">
+								<ul class="c-author-list__name--wrap">
+									<li class="c-author-list__name">Iruka</li>
+									<li class="c-author-list__image">SAMPLE<br>Coming<br>Soon</li>
+									<li class="c-author-list__color">モノクロ</li>
+									<li class="c-author-list__sns">X</li>
+								</ul>
+							</li>
+							<li class="c-author-list__lists--list">
+								<ul class="c-author-list__name--wrap">
+									<li class="c-author-list__name">Iruka</li>
+									<li class="c-author-list__image">SAMPLE<br>Coming<br>Soon</li>
+									<li class="c-author-list__color">モノクロ</li>
+									<li class="c-author-list__sns">X</li>
+								</ul>
+							</li>
+							<li class="c-author-list__lists--list">
+								<ul class="c-author-list__name--wrap">
+									<li class="c-author-list__name">Iruka</li>
+									<li class="c-author-list__image">SAMPLE<br>Coming<br>Soon</li>
+									<li class="c-author-list__color">モノクロ</li>
+									<li class="c-author-list__sns">X</li>
+								</ul>
+							</li>
+							<li class="c-author-list__lists--list">
+								<ul class="c-author-list__name--wrap">
+									<li class="c-author-list__name">Iruka</li>
+									<li class="c-author-list__image">SAMPLE<br>Coming<br>Soon</li>
+									<li class="c-author-list__color">モノクロ</li>
+									<li class="c-author-list__sns">X</li>
+								</ul>
+							</li>
+						</ul>
+					</dd>
+					<dt class="c-author-list__lists--title">
+						漫画
+					</dt>
+					<dd class="c-author-list__lists--wrap">
+						<ul class="c-author-list__lists--member">
+							<li class="c-author-list__lists--list">
+								<ul class="c-author-list__name--wrap">
+									<li class="c-author-list__name">Iruka</li>
+									<li class="c-author-list__image">SAMPLE<br>Coming<br>Soon</li>
+									<li class="c-author-list__color">モノクロ</li>
+									<li class="c-author-list__sns">X</li>
+								</ul>
+							</li>
+							<li class="c-author-list__lists--list">
+								<ul class="c-author-list__name--wrap">
+									<li class="c-author-list__name">Iruka</li>
+									<li class="c-author-list__image">SAMPLE<br>Coming<br>Soon</li>
+									<li class="c-author-list__color">モノクロ</li>
+									<li class="c-author-list__sns">X</li>
+								</ul>
+							</li>
+							<li class="c-author-list__lists--list">
+								<ul class="c-author-list__name--wrap">
+									<li class="c-author-list__name">Iruka</li>
+									<li class="c-author-list__image">SAMPLE<br>Coming<br>Soon</li>
+									<li class="c-author-list__color">モノクロ</li>
+									<li class="c-author-list__sns">X</li>
+								</ul>
+							</li>
+							<li class="c-author-list__lists--list">
+								<ul class="c-author-list__name--wrap">
+									<li class="c-author-list__name">Iruka</li>
+									<li class="c-author-list__image">SAMPLE<br>Coming<br>Soon</li>
+									<li class="c-author-list__color">モノクロ</li>
+									<li class="c-author-list__sns">X</li>
+								</ul>
+							</li>
+						</ul>
+					</dd>
+					<dt class="c-author-list__lists--title">
+						小説
+					</dt>
+					<dd class="c-author-list__lists--wrap">
+						<ul class="c-author-list__lists--member">
+							<li class="c-author-list__lists--list">
+								<ul class="c-author-list__name--wrap">
+									<li class="c-author-list__name">Iruka</li>
+									<li class="c-author-list__image">SAMPLE<br>Coming<br>Soon</li>
+									<li class="c-author-list__color">モノクロ</li>
+									<li class="c-author-list__sns">X</li>
+								</ul>
+							</li>
+							<li class="c-author-list__lists--list">
+								<ul class="c-author-list__name--wrap">
+									<li class="c-author-list__name">Iruka</li>
+									<li class="c-author-list__image">SAMPLE<br>Coming<br>Soon</li>
+									<li class="c-author-list__color">モノクロ</li>
+									<li class="c-author-list__sns">X</li>
+								</ul>
+							</li>
+							<li class="c-author-list__lists--list">
+								<ul class="c-author-list__name--wrap">
+									<li class="c-author-list__name">Iruka</li>
+									<li class="c-author-list__image">SAMPLE<br>Coming<br>Soon</li>
+									<li class="c-author-list__color">モノクロ</li>
+									<li class="c-author-list__sns">X</li>
+								</ul>
+							</li>
+							<li class="c-author-list__lists--list">
+								<ul class="c-author-list__name--wrap">
+									<li class="c-author-list__name">Iruka</li>
+									<li class="c-author-list__image">SAMPLE<br>Coming<br>Soon</li>
+									<li class="c-author-list__color">モノクロ</li>
+									<li class="c-author-list__sns">X</li>
+								</ul>
+							</li>
+						</ul>
+					</dd>
+					<dt class="c-author-list__lists--title">
+						写真
+					</dt>
+					<dd class="c-author-list__lists--wrap">
+						<ul class="c-author-list__lists--member">
+							<li class="c-author-list__lists--list">
+								<ul class="c-author-list__name--wrap">
+									<li class="c-author-list__name">Iruka</li>
+									<li class="c-author-list__image">SAMPLE<br>Coming<br>Soon</li>
+									<li class="c-author-list__color">モノクロ</li>
+									<li class="c-author-list__sns">X</li>
+								</ul>
+							</li>
+							<li class="c-author-list__lists--list">
+								<ul class="c-author-list__name--wrap">
+									<li class="c-author-list__name">Iruka</li>
+									<li class="c-author-list__image">SAMPLE<br>Coming<br>Soon</li>
+									<li class="c-author-list__color">モノクロ</li>
+									<li class="c-author-list__sns">X</li>
+								</ul>
+							</li>
+							<li class="c-author-list__lists--list">
+								<ul class="c-author-list__name--wrap">
+									<li class="c-author-list__name">Iruka</li>
+									<li class="c-author-list__image">SAMPLE<br>Coming<br>Soon</li>
+									<li class="c-author-list__color">モノクロ</li>
+									<li class="c-author-list__sns">X</li>
+								</ul>
+							</li>
+							<li class="c-author-list__lists--list">
+								<ul class="c-author-list__name--wrap">
+									<li class="c-author-list__name">Iruka</li>
+									<li class="c-author-list__image">SAMPLE<br>Coming<br>Soon</li>
+									<li class="c-author-list__color">モノクロ</li>
+									<li class="c-author-list__sns">X</li>
+								</ul>
+							</li>
+						</ul>
+					</dd>
+				</dl>
+			</article>
+			<article class="p-staff-list">
+				<h2 class="c-staff-list__title">staff</h2>
+				<dl class="p-staff-list__lists">
+					<dt class="c-staff-list__lists--title">
+						主催・デザイン
+					</dt>
+					<dd class="c-staff-list__lists--list">
+						<ul class="c-staff-list__name--wrap">
+							<li class="c-staff-list__name">はごろ(<a class="c-link" href="https://twitter.com/856_zetta">@856_zatta</a>)</li>
+						</ul>
+					</dd>
+					<dt class="c-staff-list__lists--title">
+						主催・デザイン
+					</dt>
+					<dd class="c-staff-list__lists--list">
+						<ul class="c-staff-list__name--wrap">
+							<li class="c-staff-list__name">はごろ(<a class="c-link" href="https://twitter.com/856_zetta">@856_zatta</a>)</li>
+						</ul>
+					</dd>
+					<dt class="c-staff-list__lists--title">
+						主催・デザイン
+					</dt>
+					<dd class="c-staff-list__lists--list">
+						<ul class="c-staff-list__name--wrap">
+							<li class="c-staff-list__name">はごろ(<a class="c-link" href="https://twitter.com/856_zetta">@856_zatta</a>)</li>
+						</ul>
+					</dd>
+				</dl>
+			</article>
+		</section>
+		<section class="p-information">
+			<dl class="p-information__lists">
+				<dt class="c-information__lists--title">
+					頒布イベント情報
 				</dt>
-				<dd class="c-author-list__lists--list">
-					<ul class="c-author-list__name--wrap">
-						<li class="c-author-list__name"></li>
+				<dd class="c-information__lists--list">
+					<ul class="c-information__name--wrap">
+						<li class="c-information__text">
+							2024/3/17 HARU COMIC CITY 32 / 東京<br>
+							<span class="c-text__unbold">スペース未定</span>
+						</li>
 					</ul>
 				</dd>
-				<dt class="c-author-list__lists--title">
-					漫画
+				<dt class="c-information__lists--title">
+					通信頒布情報
 				</dt>
-				<dd class="c-author-list__lists--list">
-					<ul class="c-author-list__name--wrap">
-						<li class="c-author-list__name">お名前（SNSなど）</li>
-					</ul>
-				</dd>
-				<dt class="c-author-list__lists--title">
-					小説
-				</dt>
-				<dd class="c-author-list__lists--list">
-					<ul class="c-author-list__name--wrap">
-						<li class="c-author-list__name">お名前（SNSなど）</li>
-					</ul>
-				</dd>
-				<dt class="c-author-list__lists--title">
-					写真
-				</dt>
-				<dd class="c-author-list__lists--list">
-					<ul class="c-author-list__name--wrap">
-						<li class="c-author-list__name">お名前（SNSなど）</li>
+				<dd class="c-information__lists--list">
+					<ul class="c-information__name--wrap">
+						<li class="c-information__text">
+							<span class="c-text__unbold">coming soon</span>
+						</li>
 					</ul>
 				</dd>
 			</dl>
-		</article>
-		<article class="p-staff-list">
-			<h2 class="c-staff-list__title">staff list</h2>
-			<dl class="p-staff-list__lists">
-				<dt class="c-staff-list__lists--title">
-					主催・デザイン
+		</section>
+		<section class="l-flyer-part">
+			<figure class="c-flyer__image--wrap">
+				<img class="c-flyer__image" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="フライヤー">
+			</figure>
+			<p class="c-flyer__explain">
+				フライヤーの配布にご協力してくださる方を募集しております。<br>
+				同人イベントに出展される方だけでなく、通信販売における頒布等をされる際にお品物と一緒に同封していただける方も大歓迎です。<br>
+				<br>ご協力頂ける方は下記Googleフォームよりご連絡をいただけますと幸いです。<br>
+				ご検討のほど何卒よろしくお願いいたします。
+			</p>
+			<button class="c-button">ペーパー配布ご協力フォーム</button>
+		</section>
+		<section class="l-concept-part">
+			<figure class="c-concept__image--wrap">
+				<img class="c-concept__image" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="concept-image">
+			</figure>
+			<dl class="c-concept__lists">
+				<dt class="c-concept__lists--category">
+					『閃火』タイトルコンセプト
 				</dt>
-				<dd class="c-staff-list__lists--list">
-					<ul class="c-staff-list__name--wrap">
-						<li class="c-staff-list__name">はごろ(<a class="c-link" href="https://twitter.com/856_zetta">@856_zatta</a>)</li>
+				<dd class="c-concept__lists--text">
+					銃火器を握り続けなければならなかった<br>
+					人生の在り方、<br>
+					最期のシーンで打ち上がる花火、<br>
+					そして「煇」という名前から<br>
+					「火」のニュアンスを取り入れつつ、<br>
+					彼が短い生涯の中で視聴者に見せてくれた、<br>
+					儚くも鮮烈な輝きをイメージしました。
+				</dd>
+			</dl>
+		</section>
+		<section class="l-contact">
+			<dl class="c-contact__lists">
+				<dt class="c-contact__lists--category">
+					運営公式
+				</dt>
+				<dd class="c-contact__lists--list">
+					<ul class="c-icon-list">
+						<li class="c-icon"><a href="c-icon__mail c-link">mail</a></li>
+						<li class="c-icon"><a href="c-icon__sns c-link">X</a></li>
 					</ul>
 				</dd>
-				<dt class="c-staff-list__lists--title">
-					主催・デザイン
+				<dt class="c-contact__lists--category">
+					主催（はごろ）
 				</dt>
-				<dd class="c-staff-list__lists--list">
-					<ul class="c-staff-list__name--wrap">
-						<li class="c-staff-list__name">はごろ(<a class="c-link" href="https://twitter.com/856_zetta">@856_zatta</a>)</li>
-					</ul>
-				</dd>
-				<dt class="c-staff-list__lists--title">
-					主催・デザイン
-				</dt>
-				<dd class="c-staff-list__lists--list">
-					<ul class="c-staff-list__name--wrap">
-						<li class="c-staff-list__name">はごろ(<a class="c-link" href="https://twitter.com/856_zetta">@856_zatta</a>)</li>
+				<dd class="c-contact__lists--list">
+					<ul class="c-icon-list">
+						<li class="c-icon"><a href="c-icon__mail c-link">mail</a></li>
+						<li class="c-icon"><a href="c-icon__sns c-link">X</a></li>
 					</ul>
 				</dd>
 			</dl>
-		</article>
-	</section>
-	<section class="p-information">
-		<dl class="p-information__lists">
-			<dt class="c-information__lists--title">
-				頒布イベント情報
-			</dt>
-			<dd class="c-information__lists--list">
-				<ul class="c-information__name--wrap">
-					<li class="c-information__text">
-						2024/3/17 HARU COMIC CITY 32 / 東京<br>
-						<span class="c-text__unbold">スペース未定</span>
-					</li>
-				</ul>
-			</dd>
-			<dt class="c-information__lists--title">
-				通信頒布情報
-			</dt>
-			<dd class="c-information__lists--list">
-				<ul class="c-information__name--wrap">
-					<li class="c-information__text">
-						<span class="c-text__unbold">coming soon</span>
-					</li>
-				</ul>
-			</dd>
-		</dl>
-	</section>
-	<section class="l-flyer-part">
-		<figure class="c-flyer__image--wrap">
-			<img class="c-flyer__image" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="フライヤー">
-		</figure>
-		<p class="c-flyer__explain">
-			フライヤーの配布にご協力してくださる方を募集しております。<br>
-			同人イベントに出展される方だけでなく、通信販売における頒布等をされる際にお品物と一緒に同封していただける方も大歓迎です。<br>
-			<br>ご協力頂ける方は下記Googleフォームよりご連絡をいただけますと幸いです。<br>
-			ご検討のほど何卒よろしくお願いいたします。
-		</p>
-		<button class="c-button">ペーパー配布ご協力フォーム</button>
-	</section>
-	<section class="l-concept-part">
-		<figure class="c-concept__image--wrap">
-			<img class="c-concept__image" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="concept-image">
-		</figure>
-		<dl class="c-concept__lists">
-			<dt class="c-concept__lists--category">
-				『閃火』タイトルコンセプト
-			</dt>
-			<dd class="c-concept__lists--text">
-				銃火器を握り続けなければならなかった<br>
-				人生の在り方、<br>
-				最期のシーンで打ち上がる花火、<br>
-				そして「煇」という名前から<br>
-				「火」のニュアンスを取り入れつつ、<br>
-				彼が短い生涯の中で視聴者に見せてくれた、<br>
-				儚くも鮮烈な輝きをイメージしました。
-			</dd>
-		</dl>
-	</section>
-	<section class="l-contact">
-		<dl class="c-contact__lists">
-			<dt class="c-contact__lists--category">
-				運営公式
-			</dt>
-			<dd class="c-contact__lists--list">
-				<ul class="c-icon-list">
-					<li class="c-icon"><a href="c-icon__mail c-link">mail</a></li>
-					<li class="c-icon"><a href="c-icon__sns c-link">X</a></li>
-				</ul>
-			</dd>
-			<dt class="c-contact__lists--category">
-				主催（はごろ）
-			</dt>
-			<dd class="c-contact__lists--list">
-				<ul class="c-icon-list">
-					<li class="c-icon"><a href="c-icon__mail c-link">mail</a></li>
-					<li class="c-icon"><a href="c-icon__sns c-link">X</a></li>
-				</ul>
-			</dd>
-		</dl>
-	</section>
-</main>
-<?php get_footer(); ?>
+		</section>
+	</main>
+	<?php get_footer(); ?>
