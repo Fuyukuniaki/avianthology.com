a2b72ee177463922a7702b837dda79f9d8c31d7a
Fuyukuniaki <info@a5-w.com>
2023-11-21 01:14:13 +0900

    20231121 アニメーション

--- assets/js/spinner.js
+++ assets/js/spinner.js
@@ -1,9 +1,18 @@
-$(window).on('load',function(){
-	$('#loading').delay(10000).fadeOut('slow', function(){
+// $(window).on('load',function(){
+// 	$('#loading').delay(10000).fadeOut('slow', function(){
+// 		$('body').removeClass('js-hidden');
+// 	});
+// 	$('.loading-bg').on('animationend', function() {
+// 		$('#loading').remove();
+// 	});
+// });
+
+$(window).on('pageshow',function(){
+	$('#loading').delay(1500).fadeOut('slow', function(){
 		$('body').removeClass('js-hidden');
 	});
 	$('.loading-bg').on('animationend', function() {
+		$('.loading-bg').remove();
 		$('#loading').remove();
 	});
 });
-
